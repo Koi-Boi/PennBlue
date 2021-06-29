9e57bae07bde176238775707c5811a6a6cc1e66d
William Messenger <graymonster9000@gmail.com>
2021-06-29 15:02:51 -0400

    added the ability to click the buttons and stored them

--- Simon_says_java.js
+++ Simon_says_java.js
@@ -1,6 +1,22 @@
-var pastCCs =[];
-function select(){
+var pastCCs = [];
+var Pselects = [];
+function Cselect(){
     var rand = Math.floor(Math.random() *3)+1;
     pastCCs.push(rand);
 }
 select();
+
+function Pselect(){
+   $("button1").on("click", function(){
+     Pselects.push(1) 
+   })
+   $("button2").on("click", function(){
+    Pselects.push(2) 
+   }) 
+   $("button3").on("click", function(){
+    Pselects.push(3) 
+   }) 
+   $("button2").on("click", function(){
+    Pselects.push(4) 
+   }) 
+}
