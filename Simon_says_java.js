b48938055f806c180c748773d5bf6c5209cb77c5
William Messenger <graymonster9000@gmail.com>
2021-06-30 14:27:01 -0400

    fixed comareArray function

--- Simon_says_java.js
+++ Simon_says_java.js
@@ -1,6 +1,6 @@
 
 var pastCCs = [];
-var Pselects = [];
+var pastPCs = [];
 function Cselect(){
     var rand = Math.floor(Math.random() *3)+1;
     pastCCs.push(rand);
@@ -9,44 +9,41 @@ function Cselect(){
 Cselect();
 $(document).ready(function(){
     $(".button1").on("click", function(){
-        Pselects.push("1");
+        pastPCs.push("1");
         console.log("1");
-        console.log(Pselects);
+        console.log(pastPCs);
       });
       $(".button2").on("click", function(){
-       Pselects.push("2");
+       pastPCs.push("2");
        console.log("2");
-       console.log(Pselects);
+       console.log(pastPCs);
       });
       $(".button3").on("click", function(){
-       Pselects.push("3");
+       pastPCs.push("3");
        console.log("3");
-       console.log(Pselects);
+       console.log(pastPCs);
       }); 
       $(".button4").on("click", function(){
-       Pselects.push("4");
+       pastPCs.push("4");
        console.log("4");
-       console.log(Pselects);
+       console.log(pastPCs);
       }); 
       
 });
-// function Pselect(){
   function compareArray(compArr, playerArr){
         var isTrue = true;
         // For each element of both arrays
         for(var i = 0 ; i <= compArr.length; i++){
              // compare if they are the same
                 // if they are different, return false
-            if(compArr[i] != playerArr[i]){
+            if(compArr[compArr.length] != playerArr[playerArr.length]){
                 isTrue = false;
             }
+            console.log(isTrue)
          return isTrue;
         }
-       console.log(isTrue);
+        
     }
-      compareArray(pastCCs, Pselects);
+compareArray(pastCCs, pastPCs);
+//compareArray(pastCCs, pastPCs);
 
-// }
-
-//compareArray(pastCCs, Pselects);
-//Pselect();
