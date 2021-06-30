e9fe02485aaba46ce49797c117829a14577f86f5
William Messenger <graymonster9000@gmail.com>
2021-06-30 12:03:23 -0400

    fixed the clicking of the buttons and the arrays now store properly

--- Simon_says_java.js
+++ Simon_says_java.js
@@ -4,36 +4,49 @@ var Pselects = [];
 function Cselect(){
     var rand = Math.floor(Math.random() *3)+1;
     pastCCs.push(rand);
-}
-
-function Pselect(){
-   $("button1").on("click", function(){
-     Pselects.push(1)
-     alert("gaming"); 
-   })
-   $("button2").on("click", function(){
-    Pselects.push(2) 
-   }) 
-   $("button3").on("click", function(){
-    Pselects.push(3) 
-   }) 
-   $("button2").on("click", function(){
-    Pselects.push(4) 
-   }) 
-}
-function compareArray(compArr, playerArr){
-    var isTrue = true;
-    // For each element of both arrays
-    for(var i = 0 ; i <= compArr.length; i++){
-         // compare if they are the same
-            // if they are different, return false
-        if(compArr[i] != playerArr[i]){
-            isTrue = false;
-        }
-     return isTrue;
-    }
-
+    console.log(pastCCs);
 }
 Cselect();
-Pselect();
-compareArray(pastCCs, Pselects);
+$(document).ready(function(){
+    $(".button1").on("click", function(){
+        Pselects.push("1");
+        console.log("1");
+        console.log(Pselects);
+      });
+      $(".button2").on("click", function(){
+       Pselects.push("2");
+       console.log("2");
+       console.log(Pselects);
+      });
+      $(".button3").on("click", function(){
+       Pselects.push("3");
+       console.log("3");
+       console.log(Pselects);
+      }); 
+      $(".button4").on("click", function(){
+       Pselects.push("4");
+       console.log("4");
+       console.log(Pselects);
+      }); 
+      
+});
+// function Pselect(){
+  function compareArray(compArr, playerArr){
+        var isTrue = true;
+        // For each element of both arrays
+        for(var i = 0 ; i <= compArr.length; i++){
+             // compare if they are the same
+                // if they are different, return false
+            if(compArr[i] != playerArr[i]){
+                isTrue = false;
+            }
+         return isTrue;
+        }
+       console.log(isTrue);
+    }
+      compareArray(pastCCs, Pselects);
+
+// }
+
+//compareArray(pastCCs, Pselects);
+//Pselect();
