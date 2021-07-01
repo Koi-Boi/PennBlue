f6041f4947492230ad6c9dcce39b08a1e378d05f
William Messenger <graymonster9000@gmail.com>
2021-07-01 19:34:03 -0400

    finnal product, made the buttons flash in a sequence and also added the streak counter at the bottom, it aslo nolonger needs you to input the full length before telling you that you lose

--- Simon_says_java.js
+++ Simon_says_java.js
@@ -6,50 +6,88 @@ var lastScore = 0;
 var allScores = [];
 var averageScore = 0;
 var numberOfGames = 0;
+var newItem = document.createElement("div")
+newItem.className = streak;
 //function for picking random button for the player to press
-function Cselect(){
-    pastPCs = [];
-    var rand = Math.floor(Math.random() *4)+1;
-    pastCCs.push(rand);
-   console.log(pastCCs);
+// function Cselect(){
+//     pastPCs = [];
+//     var rand = Math.floor(Math.random() *4)+1;
+//     pastCCs.push(rand);
+//    console.log(pastCCs);
    
     //for(var i=0; i<pastCCs.length; i++){
         //var selected = pastCCs[i];
-        if(rand == 1){
-         document.getElementById(rand).style.backgroundColor = "red";
-         setTimeout(function(){ document.getElementById(rand).style.backgroundColor = "#111324"; }, 1000);
-         var snd = new Audio("simonSound1.mp3"); // buffers automatically when created
-snd.play();
-        }  
+//         if(rand == 1){
+//          document.getElementById(rand).style.backgroundColor = "red";
+//          setTimeout(function(){ document.getElementById(rand).style.backgroundColor = "#111324"; }, 1000);
+//          var snd = new Audio("simonSound1.mp3"); // buffers automatically when created
+// snd.play();
+//         }  
        
-         if(rand == 2){
-         document.getElementById(rand).style.backgroundColor = "blue";
-         setTimeout(function(){ document.getElementById(rand).style.backgroundColor = "#111324"; }, 1000);
-         var snd = new Audio("simonSound2.mp3");; // buffers automatically when created
-snd.play();
-        } 
+//          if(rand == 2){
+//          document.getElementById(rand).style.backgroundColor = "blue";
+//          setTimeout(function(){ document.getElementById(rand).style.backgroundColor = "#111324"; }, 1000);
+//          var snd = new Audio("simonSound2.mp3");; // buffers automatically when created
+// snd.play();
+//         } 
        
-          if(rand == 3){
-         document.getElementById(rand).style.backgroundColor = "yellow";
-         setTimeout(function(){ document.getElementById(rand).style.backgroundColor = "#111324"; }, 1000);
-         var snd = new Audio("simonSound3.mp3"); // buffers automatically when created
-snd.play();
-        }
+//           if(rand == 3){
+//          document.getElementById(rand).style.backgroundColor = "yellow";
+//          setTimeout(function(){ document.getElementById(rand).style.backgroundColor = "#111324"; }, 1000);
+//          var snd = new Audio("simonSound3.mp3"); // buffers automatically when created
+// snd.play();
+//         }
        
-         if(rand == 4){
-         document.getElementById(rand).style.backgroundColor = "green";
-         setTimeout(function(){ document.getElementById(rand).style.backgroundColor = "#111324"; }, 1000);
-         var snd = new Audio("simonSound4.mp3"); // buffers automatically when created
-snd.play();
-        }  
+//          if(rand == 4){
+//          document.getElementById(rand).style.backgroundColor = "green";
+//          setTimeout(function(){ document.getElementById(rand).style.backgroundColor = "#111324"; }, 1000);
+//          var snd = new Audio("simonSound4.mp3"); // buffers automatically when created
+// snd.play();
+//         }  
        
-       setTimeout(function(){ document.getElementById("1").style.backgroundColor = "#111324"; }, 1000);
-       setTimeout(function(){ document.getElementById("2").style.backgroundColor = "#111324"; }, 1000);
-       setTimeout(function(){ document.getElementById("3").style.backgroundColor = "#111324"; }, 1000);
-       setTimeout(function(){ document.getElementById("4").style.backgroundColor = "#111324"; }, 1000);
-//      } 
+//        setTimeout(function(){ document.getElementById("1").style.backgroundColor = "#111324"; }, 1000);
+//        setTimeout(function(){ document.getElementById("2").style.backgroundColor = "#111324"; }, 1000);
+//        setTimeout(function(){ document.getElementById("3").style.backgroundColor = "#111324"; }, 1000);
+//        setTimeout(function(){ document.getElementById("4").style.backgroundColor = "#111324"; }, 1000);
+// //      } 
     
+//}
+function turnOn(buttonNum){
+    return (function(){
+        var color = "red";
+        var sound = "simonSound1.mp3"; 
+        if(buttonNum == 2){
+         color = "blue";
+         sound = "simonSound2.mp3";
+         }  
+         if(buttonNum == 3){
+            color = "yellow";
+            sound = "simonSound3.mp3";
+        }  
+        if(buttonNum == 4){
+            color = "green";
+            sound = "simonSound4.mp3";
+            }  
+        document.getElementById(buttonNum).style.backgroundColor = color;
+        var snd = new Audio(sound); // buffers automatically when created
+        snd.play();
+    });
 }
+function turnOff(buttonNum){
+   return (function (){ document.getElementById(buttonNum).style.backgroundColor = "#111324";}); 
+}
+function Cselect(){
+    pastPCs = [];
+     var rand = Math.floor(Math.random() *4)+1;
+    pastCCs.push(rand);
+ console.log(pastCCs);
+    for(var i=0; i<pastCCs.length;i++){
+        var buttonNum = pastCCs[i];
+        setTimeout(turnOn(buttonNum), 1000*i);
+        setTimeout(turnOff(buttonNum), 1000*(i+1))
+       }
+}
+
 //adds event listeners for clicking on the colored buttons and the start button
 $(document).ready(function(){
     $(".glow-on-hover").on("click", function(e){
@@ -60,22 +98,28 @@ $(document).ready(function(){
          pastPCs.push(1);
          console.log("1");
          console.log(pastPCs);
-         if(pastPCs.length == pastCCs.length){
-            compareArray(pastCCs, pastPCs);
-            reset();
+         streak();
+         compareArray(pastCCs, pastPCs);;
+          reset();
+        if(pastPCs.length == pastCCs.length && pastPCs.length != 0){
+            
+           
             refresh();   
-         }
+        }
        
        });
        $(".button2").on("click", function(){
         pastPCs.push(2);
         console.log("2");
         console.log(pastPCs);
-        if(pastPCs.length == pastCCs.length){
-            compareArray(pastCCs, pastPCs);
-            reset();
+        streak();
+        compareArray(pastCCs, pastPCs);
+         reset();
+       if(pastPCs.length == pastCCs.length && pastPCs.length != 0){
+            
+           
             refresh();
-         }
+        }
         
     
        });
@@ -83,9 +127,12 @@ $(document).ready(function(){
         pastPCs.push(3);
         console.log("3");
         console.log(pastPCs);
-        if(pastPCs.length == pastCCs.length){
-            compareArray(pastCCs, pastPCs);
-            reset();
+        streak();
+        compareArray(pastCCs, pastPCs);
+         reset();
+    if(pastPCs.length == pastCCs.length && pastPCs.length != 0){
+            
+           
             refresh(); 
          }
      
@@ -94,9 +141,12 @@ $(document).ready(function(){
         pastPCs.push(4);
         console.log("4");
         console.log(pastPCs);
-        if(pastPCs.length == pastCCs.length){
-            compareArray(pastCCs, pastPCs);
-            reset();
+        streak();
+        compareArray(pastCCs, pastPCs);
+         reset();
+    if(pastPCs.length == pastCCs.length && pastPCs.length != 0){
+            
+           
             refresh();
          }
         
@@ -108,7 +158,7 @@ $(document).ready(function(){
         var isTrue = true;
              // compare if the last elements are the same
                 // if they are different, return false
-            for(var i=0; i<compArr.length; i++){
+            for(var i=0; i<pastPCs.length; i++){
                 if(compArr[i] != playerArr[i]){
                     isTrue = false;
                     
@@ -148,21 +198,10 @@ function reset (){
 function refresh (){
     if(compareArray(pastCCs, pastPCs) == true){
         Cselect();
+         clicks = 0;
     } 
 }
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-        this.sound.play();
-    }
-    this.stop = function(){
-        this.sound.pause();
-    }    
+function streak (){
+newItem.innerHTML = pastCCs.length;
+document.getElementById("button5").appendChild(newItem);
 }
-
-
